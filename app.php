ac6784b86a364716e4a0278cc8618f03ce0e6651
Rade Jasenovčanin <78330407+rade901@users.noreply.github.com>
2022-10-13 09:26:49 +0200

    Questions-tool

---
+++ app.php
@@ -0,0 +1,62 @@
+<?php
+$text1 = "question:";
+$text2 = "answer1:";
+$text3 = "answer2:";
+$text4 = "answer3:";
+$text5 = "correct:";
+
+// define variables and set to empty values
+$answer1 = $answer2 = $answer3 =  $question =  $correct = $namePromt = $message = "";
+// If the filds are empty
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $answer1 = filter_var($_POST["answer1"]);
+    $answer2 = filter_var($_POST["answer1"]);
+    $answer3 = filter_var($_POST["answer1"]);
+    $question = filter_var($_POST["question"]);
+    $correct = filter_var($_POST["correct"]);
+    if ($answer1 == "") {
+        $namePromt = "Please enter your answer1";
+    }
+    if ($answer2 == "") {
+        $namePromt = "Please enter your answer2";
+    }
+    if ($answer2 == "") {
+        $namePromt = "Please enter your answer3";
+    }
+    if ($question == "") {
+        $emailPromt = "Please enter your question";
+    } else {
+        //if the filds are not empty
+        $message = "Question is save!";
+        echo "<script>setTimeout(\"location = 'index.php';\",1500);</script>";
+
+
+        // Data we'd like to write to the users.txt file
+        $lineTop = "[\n";
+        $lineMiddle = "'question' =>'$question'," . "\n";
+        $lineAnswer = "'answers' => [" . "\n";
+        $answer1 = " 1 => '$answer1', ". "\n";
+        $answer2 = " 2 => '$answer2', ". "\n";
+        $answer3 = " 3 => '$answer3', ". "\n";
+        $endOfanswer = "]," . "\n";
+        $correctAnswer = " correct => '$correct', ". "\n";
+        $lineBotton = "],\n";
+
+        // Open questions.txt for writing
+        $fh = fopen('questions.txt', 'a');
+
+        // Write the data
+        fwrite($fh, $lineTop);
+        fwrite($fh, $lineMiddle);
+        fwrite($fh, $lineAnswer);
+        fwrite($fh, $answer1);
+        fwrite($fh, $answer2);
+        fwrite($fh, $answer3);
+        fwrite($fh, $endOfanswer);
+        fwrite($fh, $correctAnswer);
+        fwrite($fh, $lineBotton);
+
+        // Close the handle
+        fclose($fh);
+    }
+}
---
+++ index.php
@@ -0,0 +1,74 @@
+<html>
+<head>
+    <title><?php echo "Question tool"; ?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body class="bg-light">
+<?php
+include_once("app.php");
+?>
+<div class="container text-center">
+    <div class="row">
+        <div class="col pt-5">
+         <img src="img/questions.jpg" width="300" height="300">
+        </div>
+        <div class="col">
+            <h1 class="pt-5 pb-5">Questions Tool</h1>
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                <div class="mb-3">
+                    <label for="question" class="form-label">Question</label>
+                    <input type="text" id="text" name="question" size="20" maxlength="40"
+                           value="<?php echo $question; ?>"
+                           placeholder="<?php echo $emailPromt; ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="answer" class="form-label">Answer 1</label>
+                    <input type="text" id="answer1" name="answer1" size="20" maxlength="40"
+                           value="<?php echo $answer1; ?>"
+                           placeholder="<?php echo $namePromt; ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="answer" class="form-label">Answer 2</label>
+                    <input type="text" id="answer2" name="answer2" size="20" maxlength="40"
+                           value="<?php echo $answer2; ?>"
+                           placeholder="<?php echo $namePromt; ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="answer" class="form-label">Answer 3</label>
+                    <input type="text" id="answer3" name="answer3" size="20" maxlength="40"
+                           value="<?php echo $answer3; ?>"
+                           placeholder="<?php echo $namePromt; ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="answer" class="form-label">Correct</label>
+                    <input type="number" id="correct" name="correct" size="3" maxlength="40"
+                           value="<?php echo $correct; ?>"
+                           placeholder="<?php echo $namePromt; ?>">
+                    <input type="submit" id="submit" name="submit" value="Save">
+                </div>
+                <p>&nbsp;</p>
+                <p>Questions.txt</p>
+                <a class="btn btn-primary" href="questions.txt" download="questions.txt">Download</a>
+            </form>
+            <?php echo $message; ?>
+        </div>
+        <div class="col">
+            <p>&nbsp;</p>
+            <div id="list">
+                <p><iframe src="questions.txt" frameborder="0" height="400"
+                           width="95%"></iframe></p>
+            </div>
+        </div>
+    </div>
+</div>
+    <script>
+        if (window.history.replaceState) {
+            window.history.replaceState(null, null, window.location.href);
+        }
+    </script>
+</body>
+
+</html
